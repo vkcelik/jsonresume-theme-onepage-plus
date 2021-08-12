b44c08fc9b46155f57591f5c98c128e64e576f27
daniel <danielrodcaball@gmail.com>
2021-08-12 17:28:54 +0200

    Theme modified based on onepage theme

--- index.js
+++ index.js
@@ -2,29 +2,55 @@ var fs = require("fs");
 var path = require('path');
 var Handlebars = require("handlebars");
 
+// Handlebars.registerHelper('formatDate', dateString =>
+//     new Date(dateString).toLocaleDateString('en', {
+//         month: 'short',
+//         year: 'numeric',
+//     }),
+// );
+
+Handlebars.registerHelper('formatDate', function(dateString) {
+    let dateStrArr = dateString.split('-');
+
+    if (dateStrArr[0] && dateStrArr[1] && dateStrArr[2])
+        return new Date(dateString).toLocaleDateString('en', {
+            month: 'short',
+            year: 'numeric',
+            day: 'numeric',
+        });
+
+    if (dateStrArr[0] && dateStrArr[1])
+        return new Date(dateString).toLocaleDateString('en', {
+            month: 'short',
+            year: 'numeric',
+        });
+
+    return dateStrArr;
+})
+
 function render(resume) {
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	var partialsDir = path.join(__dirname, 'partials');
-	var filenames = fs.readdirSync(partialsDir);
+    var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+    var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+    var partialsDir = path.join(__dirname, 'partials');
+    var filenames = fs.readdirSync(partialsDir);
 
-	filenames.forEach(function (filename) {
-	  var matches = /^([^.]+).hbs$/.exec(filename);
-	  if (!matches) {
-	    return;
-	  }
-	  var name = matches[1];
-	  var filepath = path.join(partialsDir, filename)
-	  var template = fs.readFileSync(filepath, 'utf8');
+    filenames.forEach(function(filename) {
+        var matches = /^([^.]+).hbs$/.exec(filename);
+        if (!matches) {
+            return;
+        }
+        var name = matches[1];
+        var filepath = path.join(partialsDir, filename)
+        var template = fs.readFileSync(filepath, 'utf8');
 
-	  Handlebars.registerPartial(name, template);
-	});
-	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
+        Handlebars.registerPartial(name, template);
+    });
+    return Handlebars.compile(tpl)({
+        css: css,
+        resume: resume
+    });
 }
 
 module.exports = {
-	render: render
+    render: render
 };
